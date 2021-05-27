e4623ace2391466df282e5b8f26f34f800afde2d
ELYOUSFI Mohamed <mohamed5elyousfi@gmail.com>
2021-05-27 19:03:13 +0000

    2nd commit

---
+++ seeders/20210527185509-users-create.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var faker = require('faker');
+
+const roles = ['admin','author','guest']
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    
+    for(var i=0;i<20;i++) {
+      await queryInterface.bulkInsert('users', [{
+        username:faker.name.firstName(),
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+        role: roles[Math.floor(Math.random() * roles.length)],
+        CreatedAt : new Date(),
+        updatedAt : new Date()
+      }]);
+    }
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
