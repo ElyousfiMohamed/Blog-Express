63f08aab88d8b51ea2a536360e233edb92bd3bdc
ELYOUSFI Mohamed <mohamed5elyousfi@gmail.com>
2021-05-28 21:15:46 +0000

    3rd commit

---
+++ repositories/users.js
@@ -0,0 +1,64 @@
+const { User } = require('../models')
+const user = require('../models/user')
+const { unsubscribe } = require('../routes')
+
+module.exports = {
+
+        async getAllUsers() {
+                return await User.findAll()
+        },
+        // méthodes à implémenter
+        getUsers(offset = 0, limit = 10) {
+                return User.findAll({
+                        offset : offset,
+                        limit : limit
+                })
+        },
+        getAdmins() {
+                return this.getUsersByRole('admin')
+        },
+        getAuthors() {
+                return this.getUsersByRole('author')
+        },
+        getGuests() {
+                return this.getUsersByRole('guest')
+        },
+        getUser(id) {
+                return User.findAll({
+                        where :  {
+                              id : id
+                        }
+                })
+
+        },
+        getUserByEmail(email) {
+                return User.find(email)
+        },
+        async addUser(user) {
+                var userA = await User.create(user)
+                return userA
+        },
+        async updateUser(id, user) {
+                await User.update(user, {
+                        where : {
+                                id : id
+                        }
+                })
+        },
+        async deleteUser() {
+                return await User.destroy({
+                        where : {
+                                id : id
+                        }
+                })  
+        },
+        // D'autres méthodes jugées utiles
+
+        async getUsersByRole(role) {
+                return await User.findAll({
+                        where: {
+                                role: role
+                        }
+                })
+        }
+}
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+const usersRepo = require('../repositories/users')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+  res.send(usersRepo.getAllUsers())
+})
+  .post((req,res)=>{
+    usersRepo.addUser(req.body)
+    res.send('user added')
+  })
+  .put((req,res)=>{
+    usersRepo.updateUser(req.body)
+    res.send('user updated')
 
+  })
+
+router.route('/:id')
+  .get((req,res)=>{
+    res.send(usersRepo.getUser(req.params.id))
+  })
+  .delete((req,res)=>{
+    usersRepo.deleteUser(req.params.id) 
+    res.send('Utilisateur supprimé')
+
+  })
 module.exports = router;
